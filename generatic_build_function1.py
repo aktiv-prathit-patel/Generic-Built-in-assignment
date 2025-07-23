b2a53892a86159ba19a90cdbda2d6cb419ecac7e
aktiv-prathit-patel <prathit.patel@aktivsoftware.com>
2025-07-23 10:26:46 +0530

    Update generatic_build_function1.py

--- generatic_build_function1.py
+++ generatic_build_function1.py
@@ -2,33 +2,34 @@
 inp = input("enter a series of numbers (comma-separated) as input: ")
 inp = inp.split(',')
 input_list = [int(i.strip()) for i in inp]
-print(input_list)
-original_list = input_list
+print("\nlist of integers")
+for inti in input_list:
+  print(inti)
+original_list = input_list.copy()
 
 # From the list, remove all numbers less than 10.
 less_ten_list = [i for i in input_list if i <10]
-print('less than 10 list = ',less_ten_list)
+print('\nless than 10 list = ',less_ten_list)
 
 # Check if all remaining numbers are even.
-print('even number: ',[i for i in less_ten_list if i%2 == 0])
+print('\neven number: ',[i for i in less_ten_list if i%2 == 0])
 
 # Check if any of the remaining numbers are divisible by 5
-print('divisible 5 list: ',[i for i in less_ten_list if i%5 == 0])
+print('\ndivisible 5 list: ',[i for i in less_ten_list if i%5 == 0])
 
 # Display the highest number, the lowest number, and the total of the remaining list.
-print('max number = ',max(less_ten_list),'\nmin number = ',min(less_ten_list), '\nsum of total number = ',sum(less_ten_list))
+print('\nmax number = ',max(less_ten_list),'\nmin number = ',min(less_ten_list), '\nsum of total number = ',sum(less_ten_list))
 
 # Show the list sorted in both ascending and descending order.
-print('ascending order list = ',sorted(less_ten_list),'\ndescending order list = ',sorted(less_ten_list,reverse=True))
+print('\nascending order list = ',sorted(less_ten_list),'\ndescending order list = ',sorted(less_ten_list,reverse=True))
 
 # Convert the original list into a tuple and a set, and print both.
-list_2_tuple = tuple(less_ten_list)
-list_2_set = set(less_ten_list)
-print('list into tuple:',list_2_tuple ,'\nlist into set:',list_2_set)
+list_2_tuple = tuple(original_list)
+list_2_set = set(original_list)
+print('\nlist into tuple:',list_2_tuple ,'\nlist into set:',list_2_set)
 
 # Print the type of all collections used and verify if each one belongs to its expected data type.
-print('check all types :\n',less_ten_list,' : ',type(less_ten_list),'\n',list_2_tuple,' : ',type(list_2_tuple),'\n',list_2_set,' : ',type(list_2_set))
+print('\ncheck all types :\n',original_list,' : ',type(original_list),'\n',list_2_tuple,' : ',type(list_2_tuple),'\n',list_2_set,' : ',type(list_2_set))
 
 # Pair each number in the original list with its square and display the result.
-print('list with its square: ',[i**2 for i in original_list])
-
+print('\nlist with its square: ',[i**2 for i in original_list])
