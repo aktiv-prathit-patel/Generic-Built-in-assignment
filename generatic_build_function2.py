8477e0d3239902f9c9a6c44af01f3edfb4411788
aktiv-prathit-patel <prathit.patel@aktivsoftware.com>
2025-08-04 16:41:49 +0530

    Update generatic_build_function2.py

--- generatic_build_function2.py
+++ generatic_build_function2.py
@@ -2,24 +2,30 @@
 Data = {'name':'Billy','age':39,'score':[89,47,61,50]}
 
 # Display all available operations or attributes that can be performed on this dictionary.
-print("\n===Dictionary Operations ===")
 operation_perform_data = Data.copy()
-print('Copy of the dictionary',operation_perform_data)
-print('All the values in the dictionary', operation_perform_data.values())
+
+print('\nCopy of the dictionary',operation_perform_data)
+print('\nAll the values in the dictionary', operation_perform_data.values())
+
 key = operation_perform_data.keys()
-print('List containing the dictionary keys', key)
+print('\nList containing the dictionary keys', key)
+
 items = operation_perform_data.items()
-print("List containing a tuple for each key value pair", operation_perform_data.items())
-print("Use get method for age data get: ", operation_perform_data.get('age'))
+print("\nList containing a tuple for each key value pair", operation_perform_data.items())
+print("\nUse get method for age data get: ", operation_perform_data.get('age'))
+
 operation_perform_data.setdefault('surname','Conifer')
-print("Use setdefault value: ",operation_perform_data)
+print("\nUse setdefault value: ",operation_perform_data)
+
 operation_perform_data.update({'name':"john"})
-print("Use setdefault value: ",operation_perform_data)
+print("\nUse setdefault value: ",operation_perform_data)
+
 operation_perform_data.pop('age')
-print("Use pop to remove age ", operation_perform_data)
+print("\nUse pop to remove age ", operation_perform_data)
+
 operation_perform_data.popitem()
-print('Use popitem to remove last key-pair', operation_perform_data)
-print("Use clear to remove all directory", operation_perform_data.clear())
+print('\nUse popitem to remove last key-pair', operation_perform_data)
+print("\nUse clear to remove all directory", operation_perform_data.clear())
 
 # Ask the user to enter a mathematical expression as a string and evaluate its result.
 enter_mth_str = input('\nEnter Mathematical string : ')
@@ -33,12 +39,11 @@ print('\nResult in integer', int(result),'\nresult in float :',float(result))
 list_test = ['a','b','c','d']
 string_test = " ".join(list_test)
 print("\n",list_test, "list into string", string_test)
-print(string_test, "string into list", string_test.split(" "))
+print(string_test, "string into list", string_test.split(" "),"\n")
 
 # From the "scores" list, display a subset of elements using a range of indices.
-print('\n')
 for index, value in enumerate(Data['score']):
-    print(f"Index: {index}, Value: {value}")
+    print("Index: ",index, "Value: ",value)
 
 # Loop over a range of numbers, round each to the nearest whole number, and convert to string.
 float_num = [1.2,3.12,5.78,8.90]
@@ -51,18 +56,37 @@ print('Convert string: ',''.join(string_con))
 # Access the first three elements of the "scores" list using a manual iteration approach.
 scores_first_3 = Data['score'][:3]
 scores_first_3 = iter(scores_first_3)
-try:
-    while True:
+
+print("\nIteration approach score first 3 element")
+while True:
+    try:
         element = next(scores_first_3)
-        print(element)
-except StopIteration:
-    print("End of list reached.")
+        print("",element)
+    except:
+        print("End of loop\n")
+        break
 
 # Test and display the truthiness of the following: an empty list, a non-zero number, and an empty string.
-check_list = []
-num = 1
-check_string = ""
+#Check List empty or not
+check_list = list(input("Enter list: "))
+check_list = [i for i in check_list if i != " " and i != ""]
+if bool(check_list):
+    print("Not Empty list\n")
+else:
+    print("List Empty")
+#Check number is zero or not
+try:
+    check_num= float(input("\nEnter number: "))
+    if bool(check_num):
+        print("Non Zero")
+    else:
+        print("Zero number")
+except:
+    print("Not a number")
 
-print("\nEmpty list:", bool(check_list))
-print("\nNon-zero number:", bool(num))
-print("\nEmpty string:", bool(check_string))
+#Check String or not
+check_string = input("\nEnter any string: ")
+if bool(check_string):
+    print("String not empty")
+else:
+    print("Empty string")
